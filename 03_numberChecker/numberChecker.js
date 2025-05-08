6e3c0966d68dd6270929fbdb8146af161deaed9f
edjvc <181752503+edjvc@users.noreply.github.com>
2025-05-08 16:45:52 +0800

    Complete javascript-exercises/ 02-05

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,5 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number > 9) {
     return true;
   } else {
     return false;
@@ -8,3 +8,13 @@ function numberChecker(number) {
 
 // Do not edit below this line
 module.exports = numberChecker;
+
+// alternative
+
+// function numberChecker(number) {
+//   if (number !== 9 && number !== 6) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
